96e84192a9934ea69b943d9281e9d768f70c909d
Lerdor <marshal604@gmail.com>
2019-11-09 18:08:31 +0800

    feature(crawler/crawler): init QuarterRevenueCrawler

--- server/src/utils/crawler/crawler.ts
+++ server/src/utils/crawler/crawler.ts
@@ -1,11 +1,15 @@
 import { MonthRevenueCrawler } from '@utils/crawler/month-revenue.crawler';
+import { QuarterRevenueCrawler } from '@utils/crawler/quarter-revenue.crawler';
 
 export class Crawler {
   private monthRevenueCrawler: MonthRevenueCrawler;
+  private quarterRevenueCrawler: QuarterRevenueCrawler;
   constructor() {
     this.monthRevenueCrawler = new MonthRevenueCrawler();
+    this.quarterRevenueCrawler = new QuarterRevenueCrawler();
   }
   init() {
     this.monthRevenueCrawler.init();
+    this.quarterRevenueCrawler.init();
   }
 }
