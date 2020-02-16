6089286e9f310f9367c37a3406d62372376b6ffa
Lerdor <marshal604@gmail.com>
2020-02-16 11:22:18 +0800

    fix(dialy-info.service): add get trade info method

--- server/src/services/stock/daily-info.service.ts
+++ server/src/services/stock/daily-info.service.ts
@@ -3,12 +3,17 @@ import {
   StockInfo,
   StockInfoList,
   Stock,
-  StockList
+  StockList,
+  TradeInfo,
+  TradeInfoList,
+  GetTradeInfoInput,
+  GetTradeInfoRangeListInput
 } from '@models/shared/stock';
 import { DateFormatCategory, formatDate } from '@utils/date';
-import { readFile, recursiveReadFile, RecursiveReadFileOutput } from '@utils/file';
+import { readFile, recursiveReadFile, RecursiveReadFileOutput, readDirSync } from '@utils/file';
 export class DailyInfoService {
   stockListPath = 'stock-list';
+  tradeInfoPath = 'trade-info';
 
   getCurrentStockInfo(request: GetStockInfoInput): Promise<StockInfo> {
     const time = new Date(request.startTime);
@@ -68,6 +73,64 @@ export class DailyInfoService {
     }).then(jsonData => {
       const { data }: { data: Stock[] } = JSON.parse(jsonData);
       return { list: data };
+    });
+  }
+
+  getCurrentTradeInfo(): Promise<TradeInfoList> {
+    const fileList = this.getTradeInfoFileList();
+    const lastFileName = fileList[fileList.length - 1];
+    return readFile({
+      path: this.tradeInfoPath,
+      fileName: lastFileName
+    }).then(jsonData => {
+      const { data }: { data: TradeInfo[] } = JSON.parse(jsonData);
+      return {
+        list: [
+          {
+            date: lastFileName,
+            data
+          }
+        ]
+      };
+    });
+  }
+
+  getTradeInfoRangeList(request: GetTradeInfoRangeListInput): Promise<TradeInfoList> {
+    const fileDateList = this.getDateFormatTradeInfoFileList();
+    const startTime = new Date(request.startTime);
+    const dateIndex = fileDateList.findIndex(item => item >= startTime);
+    const fileNameList = this.getTradeInfoFileList().slice(dateIndex || 0);
+    return recursiveReadFile<TradeInfo>({
+      path: this.tradeInfoPath,
+      fileNameList
+    }).then((list: RecursiveReadFileOutput<TradeInfo[]>[]) => ({
+      list
+    }));
+  }
+
+  getSpecificTradeInfoList(request: GetTradeInfoInput): Promise<TradeInfoList> {
+    return this.getTradeInfoRangeList({ startTime: request.startTime }).then(data => {
+      return {
+        list: data.list.map(obj => {
+          return {
+            date: obj.date,
+            data: obj.data.filter(item => item.code === request.code)
+          };
+        })
+      };
+    });
+  }
+
+  getTradeInfoFileList(): string[] {
+    return readDirSync(this.tradeInfoPath).map(item => item.split('.')[0]);
+  }
+
+  getDateFormatTradeInfoFileList(): Date[] {
+    return this.getTradeInfoFileList().map(item => {
+      const year = Number(item.slice(0, 4));
+      const month = Number(item.slice(4, -2)) - 1;
+      const date = Number(item.slice(-2));
+      return new Date(year, month, date);
     });
   }
 }
